a31d80d8a314fddfd9a974498e583e84921af436
Erlend Finvåg <erlend.finvag@gmail.com>
2019-02-09 03:16:47 +0100

    Add MagicMirror2

---
+++ server/config/config.js
@@ -0,0 +1,85 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
+var config = {
+  address: '0.0.0.0',
+  port: 8080,
+  ipWhitelist: ['127.0.0.1', '::ffff:127.0.0.1', '::1'],
+  language: 'en',
+  timeFormat: 24,
+  units: 'metric',
+
+  modules: [
+    {
+      module: 'alert'
+    },
+    {
+      module: 'updatenotification',
+      position: 'top_bar'
+    },
+    {
+      module: 'clock',
+      position: 'top_left'
+    },
+    {
+      module: 'calendar',
+      header: 'US Holidays',
+      position: 'top_left',
+      config: {
+        calendars: [
+          {
+            symbol: 'calendar-check',
+            url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+          }
+        ]
+      }
+    },
+    {
+      module: 'compliments',
+      position: 'lower_third'
+    },
+    {
+      module: 'currentweather',
+      position: 'top_right',
+      config: {
+        location: 'New York',
+        locationID: '5128581', // ID from http://bulk.openweathermap.org/sample/; unzip the gz file and find your city
+        appid: 'YOUR_OPENWEATHER_API_KEY'
+      }
+    },
+    {
+      module: 'weatherforecast',
+      position: 'top_right',
+      header: 'Weather Forecast',
+      config: {
+        location: 'New York',
+        locationID: '5128581', // ID from https://openweathermap.org/city
+        appid: 'YOUR_OPENWEATHER_API_KEY'
+      }
+    },
+    {
+      module: 'newsfeed',
+      position: 'bottom_bar',
+      config: {
+        feeds: [
+          {
+            title: 'New York Times',
+            url: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+          }
+        ],
+        showSourceTitle: true,
+        showPublishDate: true
+      }
+    }
+  ]
+}
+
+// DO NOT EDIT THE LINE BELOW
+if (typeof module !== 'undefined') { module.exports = config }
